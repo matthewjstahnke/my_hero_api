4893de18ac47c1ea21d68affa2b3f84cdad2312c
matthewjstahnke <matthewjstahnke@gmail.com>
2020-12-02 01:49:51 -0600

    updated title of api

---
+++ lib/services/cli.rb
@@ -0,0 +1,40 @@
+class Cli
+
+    def start
+        puts "Super Smash Bros Character veiwer!"
+        puts "Now loading............................"
+        main_menu_options
+    end
+
+    def main_menu_options
+        puts "______________________________________"
+        puts "Type 1 to See All Characters"
+        puts "Type 2 to Random Select a Character"
+        puts "Type 'exit' to exit the program"
+        puts "______________________________________"
+        main_menu
+    end
+
+    def main_menu
+        input = get_input
+
+        if input == 1
+            puts "Choose your Character"
+        elsif input == 2
+            puts "Your new Favorite Character is:"
+        elsif input == "exit"
+            puts "Exiting Program"
+        else
+            invalid_choice
+            main_menu_options
+        end
+    end
+
+    def get_input
+        print "Enter Choice:"
+        gets.chomp
+    end
+
+    def invalid_choice
+        "Invalid Choice, Try Again"
+    end
